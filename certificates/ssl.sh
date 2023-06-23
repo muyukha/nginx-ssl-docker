8eddb62c1b9772740947ed770eadda0d861befc2
Desmond <desmond.muyukha@scaratec.com>
2023-06-23 17:27:46 +0300

    update ssl script to non-interactive

--- certificates/ssl.sh
+++ certificates/ssl.sh
@@ -1,5 +1,6 @@
+
+#openssl genrsa -out privatekey.pem 2048
+#openssl req -key privatekey.pem -new -out nextresearch.csr
+#openssl x509 -signkey privatekey.pem -in nextresearch.csr -req -days 365 -out certificate.crt
 # non-interactive and 10 years expiration
-openssl genrsa -out privatekey.pem 2048
-openssl req -key privatekey.pem -new -out nextresearch.csr
-openssl x509 -signkey privatekey.pem -in nextresearch.csr -req -days 365 -out certificate.crt
-#openssl req -x509 -newkey rsa:4096 -keyout privatekey.pem -out certificate.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
+openssl req -x509 -newkey rsa:4096 -keyout privatekey.pem -out certificate.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
